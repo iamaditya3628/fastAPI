c7c5e7c04cd362d0994d16e4ad3d021f2aaad144
Aditya tiwari <iamaditya3628@gmail.com>
2024-07-24 15:47:51 +0530

    done query parameters

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from typing import Optional
 app=FastAPI() #creating an instance of FastAPI
 
 #what is an end point..
@@ -33,9 +33,9 @@ app=FastAPI() #creating an instance of FastAPI
 # now we are going to learn about path parameters in fastapi...
 
 #suppose we have to fetch a blog list
-@app.get("/")
-async def index():
-    return {"data": "Blog list..."}
+# @app.get("/blog")
+# async def index():
+#     return {"data": "Blog list..."}
 
 #above function will show {"data" : "blog list"}
 #now we have to fetch a perticular blog info from the blog list.
@@ -52,8 +52,31 @@ async def unpublished():                       #firstly it will match with the l
     return {"data":"unpublished blogs."}       #here we have defined it above dynamic id. and now it is error free
 
 @app.get('/blog/{id}')          # Here we are passing the id dynamically.
-async def show_blog(id: int):
+async def show_blog(id: int):  #here this step is very much similar to thr type conversion, this sort of things are managed by pidentics.
     return {"data": "Blog info", "id": id}
+
+
+# Now we are going to see API docs which are swegger and redocs
+
+# creating API in other frameworks is somehow tough beause some time we dont have any frontend or way to test the API ,
+# # but fastAPI provides the automaitc documentations called swegger ui and redoc where we can test our API whenever needed. 
+
+
+
+# QUERY Parameters:
+
+# supposse we have a database which contains a blog. So fetching all the blogs from the database is super inefficient, so here we have to limit those things.
+# So to solve these things the query parameter was introduced. Syntax= "/blog?limit=0&"published"=true. example:
+
+@app.get("/blog")
+async def index(limit=10,published: bool=True, sort: Optional[str]=None):                  # hre we have to note one thing that if we do not define the type of published than the value of published is always true even if we give published=false because every thing we give in path or end point is accepted as string
+        if published:                                    #We can also give the default value to the published and the limit in order to get no error if these values are not passed. 
+                return{f"{limit} published blogs are fetched from the db."}  #We can accept the optional parameter also, for that we have to import optional from typing
+
+        else:
+                 return {"data": f"{limit} blogs fetched from the database."}
+        
+  
 
 
 
